239390b8f38e60b52ac86715f3157c57095670b1
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-09 20:12:15 -0400

    add mocha tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,21 +6,21 @@ const request = require('request');
 const fetchBreedDescription = function(breedName, callback) {
 
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, function(error, response, body) {
-   
+
     const data = JSON.parse(body);
 
     if (error) {
 
       return callback(error, null);
-      
+
     }
 
     if (!data.length) {
       return callback('Invalid', null);
 
-    } else {
-      callback(null, data[0].description);
     }
+    
+    return callback(null, data[0].description);
 
   });
 
